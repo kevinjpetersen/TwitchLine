2d75775d8b9b083822f9b8a9cb7a888c90548ea1
Kevin J. Petersen <kevingeeken@gmail.com>
2020-01-01 21:42:57 +0100

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# TwitchLine
+Command Line with integrated Twitch that only plays when it's running commands to take full advantage of work and fun!
---
+++ TwitchLine/Data/TwitchLineService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitchLine.Data
+{
+    public class TwitchLineService
+    {
+        public Process CmdProcess { get; set; }
+
+        public TwitchLineService()
+        {
+
+        }
+    }
+}
